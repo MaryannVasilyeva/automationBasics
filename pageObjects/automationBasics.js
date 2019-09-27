f71399db71fe1c3e4a6edd8d978954b6fc2ad308
Maryann Vasilyeva <maryannvasilyeva@Maryanns-MacBook-Pro.local>
2019-09-27 15:57:16 -0600

    added the even and odd testing

--- pageObjects/automationBasics.js
+++ pageObjects/automationBasics.js
@@ -1,3 +1,39 @@
 module.exports = {
-    
+    url: ('https://devmountain-qa.github.io/Automation-Basics/build/'),
+    elements: {
+    //OVERALL LANDING PAGE DIVS
+        landingPage: ('#root'),
+        evensAndOdds: {
+            selector: '//div[@class="puzzleBox evenAndOddPB"]//h4',
+            locateStrategy: 'xpath'
+        },
+        filterObject: {
+            selector: '//div[@class="puzzleBox filterObjectPB"]//h4',
+            locateStrategy: 'xpath'
+        },
+        filterString: {
+            selector: '//div[@class="puzzleBox filterStringPB"]//h4',
+            locateStrategy: 'xpath'
+        },
+        palindrome: {
+            selector: '//div[@class="puzzleBox palindromePB"]//h4',
+            locateStrategy: 'xpath'
+        },
+        sum: {
+            selector: '//div[@class="puzzleBox sumPB"]//h4',
+            locateStrategy: 'xpath'
+        },
+    //BUTTONS
+        evenOddButton: '[name="evenOddButton"]',
+        objectFilterButton: '[name="objectFilterButton"]',
+        stringFilterButton: '#nameFilterButton',
+        palindromeButton: '[name="palindromeButton"]',
+        sumButton: '[name="sumButton"]',
+    //INPUTS
+        evenOddInput: '[name="evenOddInput"]',
+    //RESULTS
+        oddResults: '[name="oddResults"]',
+        evenResults: '[name="evenResults"]'
+    }
+
 }
--- tests/automationBasics.js
+++ tests/automationBasics.js
@@ -1,3 +1,28 @@
+var automationBasics
 module.exports = {
-    
+    beforeEach: browser => {
+        automationBasics = browser.page.automationBasics()
+        automationBasics.navigate()
+    },
+    'Testing the basics - loading page': browser => {
+        automationBasics
+            .waitForElementVisible('@landingPage')
+            automationBasics.expect.element('@evensAndOdds').text.to.equal("Evens and Odds")
+            automationBasics.expect.element('@filterObject').text.to.equal("Filter Object")
+            automationBasics.expect.element('@filterString').text.to.equal("Filter String")
+            automationBasics.expect.element('@palindrome').text.to.equal("Palindrome")
+            automationBasics.expect.element('@sum').text.to.equal("Sum")
+    },
+    'Testing Evens and Odds': browser => {
+        automationBasics
+            .setValue('@evenOddInput', '5')
+            .click('@evenOddButton')
+            automationBasics.expect.element('@oddResults').text.to.equal("Odds: [5]")
+            automationBasics.expect.element('@evenResults').text.to.equal("Evens: []")
+            automationBasics.clearValue('@evenOddInput')
+            .setValue('@evenOddInput', '6')
+            .click('@evenOddButton')
+            automationBasics.expect.element('@evenResults').text.to.equal("Evens: [6]")
+            automationBasics.expect.element('@oddResults').text.to.equal("Odds: []")
+    }
 }
